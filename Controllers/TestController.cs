ff1cf1bc081dbb9c151cc846acfa6c4dc45daafd
RazanAbdAlghani <rozza5353@gmail.com>
2025-04-25 23:43:38 +0300

    Test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationFlowSync.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
